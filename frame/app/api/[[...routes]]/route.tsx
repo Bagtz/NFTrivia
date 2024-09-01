032ab3ef324814b17b8baa7b9264580ea053b669
Thiago Galante Pereira <thigalante@gmail.com>
2024-09-01 14:17:03 -0300

    user interactions

--- frame/app/api/[[...routes]]/route.tsx
+++ frame/app/api/[[...routes]]/route.tsx
@@ -1,25 +1,32 @@
 /** @jsxImportSource frog/jsx */
 
-import { Button, Frog, TextInput } from 'frog'
+import { Button, Frog, TextInput, parseEther } from 'frog'
 import { devtools } from 'frog/dev'
 // import { neynar } from 'frog/hubs'
 import { handle } from 'frog/next'
 import { serveStatic } from 'frog/serve-static'
+import { pinata } from 'frog/hubs'
+import { neynar } from 'frog/middlewares'
+import { getDisplayName } from 'next/dist/shared/lib/utils'
 
 const app = new Frog({
+  // browserLocation: '/',
   assetsPath: '/',
   basePath: '/api',
-  // Supply a Hub to enable frame verification.
-  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
-  title: 'Frog Frame',
-})
-
-// Uncomment to use Edge Runtime
-// export const runtime = 'edge'
+  title: 'ArtistQuiz',
+  hub: pinata(),
+}).use(
+  neynar({
+    apiKey: 'NEYNAR_FROG_FM',
+    features: ['interactor', 'cast'],
+  }),
+)
 
 app.frame('/', (c) => {
-  const { buttonValue, inputText, status } = c
-  const fruit = inputText || buttonValue
+  const { inputText, status } = c
+  const buttonValue = c.buttonValue
+  const started = buttonValue
+
   return c.res({
     image: (
       <div
@@ -51,37 +58,161 @@ app.frame('/', (c) => {
             whiteSpace: 'pre-wrap',
           }}
         >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
+          Let's start?
         </div>
       </div>
     ),
     intents: [
-      <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="apples">Apples</Button>,
-      <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
+      <Button
+       action="/like"
+       value="yes"
+      >
+        yes
+      </Button>,
+      <Button
+       action="/like"
+       value="no"
+      >
+        no
+      </Button>,
       status === 'response' && <Button.Reset>Reset</Button.Reset>,
     ],
   })
 })
 
+app.frame('/like', (c) => {
+  const { buttonValue, inputText, status, frameData, verified } = c
+  const artist = inputText || buttonValue
+
+  console.log('verified', verified)
+  console.log('frameData', frameData)
+
+  const { fid } = frameData || {}
+
+  const { displayName, pfpUrl } = c.var.interactor || {}
+
+  // const { displayName, followerCount, pfpUrl } = c.var.interactor || {}
+
+  // console.log('diplayName', displayName)
+  // console.log('followerCount', followerCount)
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background:
+            status === 'response'
+              ? 'linear-gradient(to right, #432889, #17101F)'
+              : 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+           {status === 'response'
+            ? `Choose your favourite artist?` 
+            : ''} 
+        </div>
+      </div>
+    ),
+    intents: [
+      <TextInput //if we dont have the input artist on our site, move for an error frame
+      placeholder="Enter your favourite artist"></TextInput>,
+      <Button
+       action="/yes"
+       value="Michael Jackson"
+      >
+        Michael Jackson
+      </Button>,
+      <Button
+       action="/no"
+       value="Madonna"
+      >
+        Madonna
+      </Button>,
+      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+    ],
+  })
+})
+
+app.frame('/yes', (c) => {
+  return c.res({
+    image: 'https://i.ytimg.com/vi/R3UACX5eULI/maxresdefault.jpg',
+    intents: [
+      <Button value="eduardo" action="/neynar">Neynar</Button>
+    ]
+  })
+})
+
+app.frame('/neynar', (c) => {
+const { displayName, followerCount, pfpUrl } = c.var.interactor || {} 
+console.log('cast: ', c.var.cast)
+console.log('interactor: ', c.var.interactor)
+return c.res({
+image: (
+<div
+style={{
+  alignItems: 'center',
+  color: 'black',
+  display: 'flex',
+  justifyContent: 'center',
+  fontSize: 48,
+  height: '100%',
+  width: '100%',
+}}
+>
+Greetings {displayName}, you have {followerCount} followers.
+{/* <img
+ style={{
+  width: 200,
+  height: 200,
+ }}
+ src={pfpUrl}
+ /> */}
+</div>
+),
+})
+})
+
+app.frame('/no', (c) => {
+  return c.res({
+    image: (
+      <div style={{color: 'black', display: 'flex', fontSize:60 }}>
+        perform a transaction
+      </div>
+    ),
+    intents: [
+      <TextInput placeholder="Value (ETH)" />,
+      <Button.Transaction target='Value'>Mint</Button.Transaction>,
+    ]
+  })
+})
+
+// app.transaction('/send-ether', (c) => {
+//   const { address } = c
+//   const { initialPath } = c
+//   const { inputText } = c
+//   return c.send({/* */})
+// })
+
 devtools(app, { serveStatic })
 
 export const GET = handle(app)
 export const POST = handle(app)
-
-// NOTE: That if you are using the devtools and enable Edge Runtime, you will need to copy the devtools
-// static assets to the public folder. You can do this by adding a script to your package.json:
-// ```json
-// {
-//   scripts: {
-//     "copy-static": "cp -r ./node_modules/frog/_lib/ui/.frog ./public/.frog"
-//   }
-// }
-// ```
-// Next, you'll want to set up the devtools to use the correct assets path:
-// ```ts
-// devtools(app, { assetsPath: '/.frog' })
-// ```
