e7b7f3b44d941f649bb76c32fdcd47d4a399f19f
Thiago Galante Pereira <thigalante@gmail.com>
2024-09-01 21:08:20 -0300

    frame almost ready - need to fix link problems when choosing artist name

--- frame/app/api/[[...routes]]/route.tsx
+++ frame/app/api/[[...routes]]/route.tsx
@@ -27,6 +27,11 @@ const findArtist = (name: string) => {
   return mockArtistList.artists.find((artist) => artist.name === name)
 };
 
+const getRandomQuestion = (max: number) => {
+  const randomIndex = Math.floor(Math.random()*(max));
+  return randomIndex;
+};
+
 app.frame('/', (c) => {
   const { inputText, status } = c
   const buttonValue = c.buttonValue
@@ -72,15 +77,8 @@ app.frame('/', (c) => {
        action="/like"
        value="yes"
       >
-        yes
+        Yes
       </Button>,
-      <Button
-       action="/like"
-       value="no"
-      >
-        no
-      </Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
     ],
   })
 })
@@ -140,62 +138,385 @@ app.frame('/like', (c) => {
     intents: [
       <TextInput //if we dont have the input artist on our site, move for an error frame
       placeholder="Enter your favourite artist"></TextInput>,
+      <Button action='/artist'>Submit</Button>,
     ],
   })
 })
 
-app.frame('/yes', (c) => {
-  return c.res({
-    image: 'https://i.ytimg.com/vi/R3UACX5eULI/maxresdefault.jpg',
-    intents: [
-      <Button value="eduardo" action="/neynar">Neynar</Button>
-    ]
-  })
-})
+app.frame('/artist', (c) => {
+  const { inputText } = c
+  const artist = inputText || ''
 
-app.frame('/neynar', (c) => {
-const { displayName, followerCount, pfpUrl } = c.var.interactor || {} 
-console.log('cast: ', c.var.cast)
-console.log('interactor: ', c.var.interactor)
-return c.res({
-image: (
-<div
-style={{
-  alignItems: 'center',
-  color: 'black',
-  display: 'flex',
-  justifyContent: 'center',
-  fontSize: 48,
-  height: '100%',
-  width: '100%',
-}}
->
-Greetings {displayName}, you have {followerCount} followers.
-{/* <img
- style={{
-  width: 200,
-  height: 200,
- }}
- src={pfpUrl}
- /> */}
-</div>
-),
-})
-})
+  const artistData = findArtist(artist)
+  if (!artistData) {
+    return c.res({
+      image: (
+        <div
+          style={{
+            alignItems: 'center',
+            background: 'black',
+            backgroundSize: '100% 100%',
+            display: 'flex',
+            flexDirection: 'column',
+            flexWrap: 'nowrap',
+            height: '100%',
+            justifyContent: 'center',
+            textAlign: 'center',
+            width: '100%',
+          }}
+        >
+          <div
+            style={{
+              color: 'white',
+              fontSize: 60,
+              fontStyle: 'normal',
+              letterSpacing: '-0.025em',
+              lineHeight: 1.4,
+              marginTop: 30,
+              padding: '0 120px',
+              whiteSpace: 'pre-wrap',
+            }}
+          >
+            Artist not found
+          </div>
+        </div>
+      ),
+      intents: [
+        <Button.Reset>Reset</Button.Reset>,
+      ],
+    })
+  }
 
-app.frame('/no', (c) => {
   return c.res({
     image: (
-      <div style={{color: 'black', display: 'flex', fontSize:60 }}>
-        perform a transaction
+      <div
+        style={{
+          alignItems: 'center',
+          background: 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          Let's start the quiz about {artistData.name}?
+        </div>
       </div>
     ),
     intents: [
-      <TextInput placeholder="Value (ETH)" />,
-      <Button.Transaction target='Value'>Mint</Button.Transaction>,
+      <Button action="/question" value={artistData.name}>Start</Button>
     ]
   })
 })
+
+app.frame('/question', (c) => {
+  const { buttonValue } = c
+  const name = buttonValue || ''
+  const artistData = findArtist(name) || mockArtistList.artists[0]
+  const randomType = getRandomQuestion(2)
+  if (randomType == 0) {
+    const randomQuestion = getRandomQuestion(artistData.textQuestions.length)
+    return c.res({
+      image: (
+        <div
+          style={{
+            alignItems: 'center',
+            background: 'black',
+            backgroundSize: '100% 100%',
+            display: 'flex',
+            flexDirection: 'column',
+            flexWrap: 'nowrap',
+            height: '100%',
+            justifyContent: 'center',
+            textAlign: 'center',
+            width: '100%',
+          }}
+        >
+          <div
+            style={{
+              color: 'white',
+              fontSize: 60,
+              fontStyle: 'normal',
+              letterSpacing: '-0.025em',
+              lineHeight: 1.4,
+              marginTop: 30,
+              padding: '0 120px',
+              whiteSpace: 'pre-wrap',
+            }}
+          >
+            {artistData.textQuestions[randomQuestion].text}
+          </div>
+        </div>
+      ),
+      intents: [
+        <TextInput placeholder="Enter your answer"></TextInput>,
+        <Button action="/textAnswer" value={artistData.textQuestions[randomQuestion].correctAnswer}>Submit</Button>
+      ]
+    })
+  }
+    const randomQuestion = getRandomQuestion(artistData.choiceQuestions.length)
+    const buttons = [];
+    for (const option of artistData.choiceQuestions[randomQuestion].options) {
+      if (option.id === artistData.choiceQuestions[randomQuestion].correctOptionId) {
+        buttons.push(
+          <Button action="/choiceAnswer" value="correct">{option.text}</Button>
+        );
+      } else {
+        buttons.push(
+          <Button action="/choiceAnswer" value="incorrect">{option.text}</Button>
+        );
+      }
+    }
+    return c.res({
+      image: (
+        <div
+          style={{
+            alignItems: 'center',
+            background: 'black',
+            backgroundSize: '100% 100%',
+            display: 'flex',
+            flexDirection: 'column',
+            flexWrap: 'nowrap',
+            height: '100%',
+            justifyContent: 'center',
+            textAlign: 'center',
+            width: '100%',
+          }}
+        >
+          <div
+            style={{
+              color: 'white',
+              fontSize: 60,
+              fontStyle: 'normal',
+              letterSpacing: '-0.025em',
+              lineHeight: 1.4,
+              marginTop: 30,
+              padding: '0 120px',
+              whiteSpace: 'pre-wrap',
+            }}
+          >
+            {artistData.choiceQuestions[randomQuestion].text}
+          </div>
+        </div>
+      ),
+      intents: [
+        ...buttons
+      ]
+    })
+})
+
+app.frame('/textAnswer', (c) => {
+  const { buttonValue, inputText } = c;
+  const correctAnswer = buttonValue;
+  const answer = inputText;
+
+  if (answer === correctAnswer) {
+    return c.res({
+      image: (
+        <div
+          style={{
+            alignItems: 'center',
+            background: 'black',
+            backgroundSize: '100% 100%',
+            display: 'flex',
+            flexDirection: 'column',
+            flexWrap: 'nowrap',
+            height: '100%',
+            justifyContent: 'center',
+            textAlign: 'center',
+            width: '100%',
+          }}
+        >
+          <div
+            style={{
+              color: 'white',
+              fontSize: 60,
+              fontStyle: 'normal',
+              letterSpacing: '-0.025em',
+              lineHeight: 1.4,
+              marginTop: 30,
+              padding: '0 120px',
+              whiteSpace: 'pre-wrap',
+            }}
+          >
+            Correct! Claim your NFT above!
+          </div>
+        </div>
+      ),
+      intents: [
+        <Button>Claim</Button>
+      ]
+    });
+  }
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background: 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          Incorrect! Try again next time!
+        </div>
+      </div>
+    ),
+  });
+});
+
+app.frame('/choiceAnswer', (c) => {
+  const { buttonValue } = c;
+  if (buttonValue === 'correct') {
+    return c.res({
+      image: (
+        <div
+          style={{
+            alignItems: 'center',
+            background: 'black',
+            backgroundSize: '100% 100%',
+            display: 'flex',
+            flexDirection: 'column',
+            flexWrap: 'nowrap',
+            height: '100%',
+            justifyContent: 'center',
+            textAlign: 'center',
+            width: '100%',
+          }}
+        >
+          <div
+            style={{
+              color: 'white',
+              fontSize: 60,
+              fontStyle: 'normal',
+              letterSpacing: '-0.025em',
+              lineHeight: 1.4,
+              marginTop: 30,
+              padding: '0 120px',
+              whiteSpace: 'pre-wrap',
+            }}
+          >
+            Correct! Claim your NFT above!
+          </div>
+        </div>
+      ),
+      intents: [
+        <Button>Claim</Button>
+      ]
+    });
+  }
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background: 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          Incorrect! Try again next time!
+        </div>
+      </div>
+    ),
+  });
+});
+
+// app.frame('/neynar', (c) => {
+// const { displayName, followerCount, pfpUrl } = c.var.interactor || {} 
+// console.log('cast: ', c.var.cast)
+// console.log('interactor: ', c.var.interactor)
+// return c.res({
+// image: (
+// <div
+// style={{
+//   alignItems: 'center',
+//   color: 'black',
+//   display: 'flex',
+//   justifyContent: 'center',
+//   fontSize: 48,
+//   height: '100%',
+//   width: '100%',
+// }}
+// >
+// Greetings {displayName}, you have {followerCount} followers.
+// {/* <img
+//  style={{
+//   width: 200,
+//   height: 200,
+//  }}
+//  src={pfpUrl}
+//  /> */}
+// </div>
+// ),
+// })
+// })
+
+// app.frame('/no', (c) => {
+//   return c.res({
+//     image: (
+//       <div style={{color: 'black', display: 'flex', fontSize:60 }}>
+//         perform a transaction
+//       </div>
+//     ),
+//     intents: [
+//       <TextInput placeholder="Value (ETH)" />,
+//       <Button.Transaction target='Value'>Mint</Button.Transaction>,
+//     ]
+//   })
+// })
 
 // app.transaction('/send-ether', (c) => {
 //   const { address } = c
